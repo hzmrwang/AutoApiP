c8dc5de33d375297a2c3d1abe14dc03da06d7aca
hzmrwang <1083596145@qq.com>
2021-03-22 03:02:03 +0800

    Update runapi.py

--- runapi.py
+++ runapi.py
@@ -91,6 +91,7 @@ for a in range(1, int(app_num)+1):
         client_id=os.getenv('CLIENT_ID_'+str(a))
         client_secret=os.getenv('CLIENT_SECRET_'+str(a))
         ms_token=os.getenv('MS_TOKEN_'+str(a))
+        print(ms_token)
         access_token_list[a-1]=getmstoken(ms_token,client_id,client_secret)
 
 #随机api序列

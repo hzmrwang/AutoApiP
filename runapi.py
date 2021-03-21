f88efea1a68a8f1b31d179ae1fa6eca3f983a530
hzmrwang <1083596145@qq.com>
2021-03-22 02:57:22 +0800

    Update runapi.py

--- runapi.py
+++ runapi.py
@@ -57,6 +57,7 @@ def getmstoken(ms_token):
         }
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
+    print(jsontxt)
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     return access_token
